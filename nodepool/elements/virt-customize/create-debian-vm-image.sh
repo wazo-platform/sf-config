ef24e9cf7e8ed56950f93f3e87ac25d9c45a33f8
Frederic Lepied <flepied@wazo.io>
2019-08-19 16:35:14 +0200

    nodepool/elements/virt-customize: create qcow2 files with a script

---
+++ nodepool/elements/virt-customize/create-debian-vm-image.sh
@@ -0,0 +1,178 @@
+#!/bin/bash
+
+set -e
+set -x
+
+fatal_error() {
+    echo "$*" 1>&2
+    exit 1
+}
+
+do_cleanup() {
+    ret=$?
+    trap - 0
+    set +e
+
+    rm -rf "$mdir/"dev/*
+    umount "$mdir/"proc
+    umount "$mdir"
+
+    losetup -d $dev
+    try=5
+    while [ $try -gt 0 ] && ! kpartx -d "$disk"; do
+	sleep 1
+	try=$(($try - 1))
+    done
+    losetup -d $disk
+    rmdir "$mdir"
+}
+
+check_binary() {
+    type -p $1 || fatal_error "$1 is missing"
+}
+
+do_chroot() {
+    local chdir="$1"
+    shift
+    PATH=/bin/:/sbin:$PATH LANG=C LC_ALL=C LC_CTYPE=C LANGUAGE=C chroot "$chdir" "$@"
+}
+
+if [ $(whoami) != root ]; then
+    fatal_error "restart as root like sudo $0 $*"
+fi
+
+dir=$(cd $(dirname $0); pwd)
+
+if [ $# != 1 -a $# != 2 ]; then
+    fatal_error "Usage: $0 <distro name> [custom script]"
+fi
+
+distro=$1
+img=$distro.raw
+custom="$2"
+
+# compress image in qcow2
+comp_opt=-c
+# no compression of the image in qcow2
+#comp_opt=
+
+export DEBOOTSTRAP_DIR=$dir/debootstrap
+
+if [ -r $dir/config ]; then
+    . $dir/config
+fi
+
+FSTYPE=${FSTYPE:=ext2}
+
+modprobe loop
+
+for cmd in git du dd parted chroot kpartx qemu-img losetup mkfs.$FSTYPE rsync; do
+    check_binary $cmd
+done
+
+if [ ! -d $DEBOOTSTRAP_DIR/.git ]; then
+    rm -rf $DEBOOTSTRAP_DIR
+    git clone https://salsa.debian.org/installer-team/debootstrap.git $DEBOOTSTRAP_DIR
+else
+    pushd $DEBOOTSTRAP_DIR
+    git pull
+    popd
+fi
+
+rm -rf $distro
+mkdir -p $distro
+
+$dir/debootstrap/debootstrap --include linux-image-amd64,grub-pc,bash --arch amd64 $distro $distro http://deb.debian.org/debian
+
+# some scriptlets need /proc
+mount --bind /proc $distro/proc
+
+# run the custom script if any
+if [ -x "$custom" ]; then
+    set +e
+    "$custom" $distro
+    rc=$?
+    set -e
+fi
+
+umount $distro/proc
+
+if [ $rc != 0 ]; then
+    exit $rc
+fi
+
+# cleanup distro
+do_chroot $distro apt-get autoremove -y
+
+rm -f $distro/var/cache/apt/archives/*.deb
+
+# compute size of the directory
+size=$(du -s -BM "$distro" | cut -f1 | sed -s 's/.$//')
+
+# add 30% to be sure that metadata from the filesystem fit
+size=$(($size * 130 / 100))
+
+# Create the image file
+rm -f $img
+fallocate -l $(($size * 1024 * 1024)) $img || dd if=/dev/zero of=$img count=$size bs=1M
+
+disk=$(losetup --show --find "$img")
+parted -s "$disk" mklabel msdos
+parted -s "$disk" mkpart primary $FSTYPE 32k '100%'
+parted "$disk" set 1 boot on
+
+part=/dev/mapper/$(kpartx -av $disk|cut -f3 -d' ')
+
+try=5
+while [ $try -gt 0 -a ! -b $part ]; do
+    sleep 1
+    try=$(($try - 1))
+done
+
+# create filesystem
+rsync -aX --delete-before --exclude=shm /dev/ $distro/dev/
+do_chroot "$distro" mkfs.$FSTYPE "$part"
+mdir=$(mktemp -d -p .)
+dev=$(losetup --show --find "$part")
+mount "$dev" "$mdir"
+
+trap do_cleanup 0
+
+rsync -a "$distro/" "$mdir/"
+
+# Let's create a copy of the current /dev
+mkdir -p "${mdir}/"/dev/pts
+rsync -a --delete-before --exclude=shm /dev/ ${mdir}/dev/
+
+# Mount /proc
+mkdir -p "${mdir}/proc"
+mount -t proc none "$mdir/proc"
+
+# Configure Grub
+uuid=$(blkid -s UUID -o value "$part")
+export grub_device_uuid=$uuid
+export grub_device=$disk
+
+cat > "$mdir"/boot/grub/device.map <<EOF
+(hd0) $disk
+EOF
+
+# hack to have a correct part dev but don't know why
+(cd $mdir/dev/; ln -sf $part $(basename $part))
+
+# install grub
+do_chroot "$mdir" grub-install --modules="ext2 xfs part_msdos xfs" --no-floppy "$disk"
+
+do_chroot "$mdir" grub-mkconfig -o /boot/grub/grub.cfg
+
+# add / to fstab
+fs_options="errors=remount-ro,nobarrier,noatime,nodiratime"
+echo "UUID=$uuid / $FSTYPE $fs_options 0 1" >> $mdir/etc/fstab
+
+trap - 0
+do_cleanup
+
+qemu-img convert $comp_opt -O qcow2 "$img" "$distro".qcow2
+rm -f $img
+
+# create-debian-vm-image.sh ends here
---
+++ nodepool/elements/virt-customize/zuul.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+set -x
+set -e
+
+# outside chroot
+if [ $# == 1 ]; then
+    dir="$1"
+    cp /var/lib/nodepool/.ssh/zuul_rsa.pub "$dir/tmp/authorized_keys"
+    cp $0 $dir/tmp/custom
+    exec chroot "$dir" /tmp/custom
+fi
+
+# inside chroot
+
+apt update -y
+
+apt install -y openssh-server sudo cloud-init cloud-initramfs-growroot cloud-utils
+
+apt install -y apt-transport-https ca-certificates gnupg2 curl locales
+
+curl -s https://download.docker.com/linux/debian/gpg > /tmp/docker.gpg
+apt-key add /tmp/docker.gpg
+
+if [[ $(cat /etc/debian_version) =~ ^9\. ]]; then
+    # debian 9
+    PKGS="rsync iproute git virtualenv python-pip python3-pip traceroute libpq-dev python2.7 libpython2.7-dev python-pkg-resources python3.5 libpython3.5-dev python3-pkg-resources sudo docker-ce docker-ce-cli containerd.io devscripts dirmngr build-essential"
+    debian_distro=stretch
+else
+    # debian 10
+    PKGS="rsync iproute2 git python-pip python3-pip traceroute libpq-dev python2.7 libpython2.7-dev python-pkg-resources python3.7 libpython3.7-dev python3-pkg-resources python-virtualenv python3-virtualenv sudo docker-ce docker-ce-cli containerd.io devscripts dirmngr build-essential"
+    debian_distro=buster
+fi
+
+cat > /etc/apt/sources.list.d/docker.list <<EOF
+deb [arch=amd64] https://download.docker.com/linux/debian $debian_distro stable
+EOF
+
+apt update -y
+
+apt install -y $PKGS
+
+apt upgrade -y
+
+pip install tox
+
+useradd -m zuul-worker -G docker -s /bin/bash
+sed -i "s/zuul-worker:\!:/zuul-worker:*:/" /etc/shadow
+mkdir /home/zuul-worker/.ssh
+chmod 0700 /home/zuul-worker/.ssh
+cp /tmp/authorized_keys /home/zuul-worker/.ssh/
+chmod 0600 /home/zuul-worker/.ssh/authorized_keys
+echo 'PATH="/home/zuul-worker/.local/bin:$PATH"' >> /home/zuul-worker/.profile
+mkdir -p /home/zuul-worker/.local/bin
+chown -R zuul-worker:zuul-worker /home/zuul-worker
+
+cat > /etc/ssh/sshd_config <<EOF
+HostKey /etc/ssh/ssh_host_rsa_key
+HostKey /etc/ssh/ssh_host_ecdsa_key
+HostKey /etc/ssh/ssh_host_ed25519_key
+KexAlgorithms curve25519-sha256@libssh.org,ecdh-sha2-nistp521,ecdh-sha2-nistp384,ecdh-sha2-nistp256,diffie-hellman-group-exchange-sha256
+Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
+MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com
+SyslogFacility AUTHPRIV
+AuthorizedKeysFile .ssh/authorized_keys
+PasswordAuthentication no
+ChallengeResponseAuthentication no
+GSSAPIAuthentication no
+GSSAPICleanupCredentials no
+UsePAM yes
+X11Forwarding no
+UseDNS no
+AcceptEnv LANG LC_CTYPE LC_NUMERIC LC_TIME LC_COLLATE LC_MONETARY LC_MESSAGES
+AcceptEnv LC_PAPER LC_NAME LC_ADDRESS LC_TELEPHONE LC_MEASUREMENT
+AcceptEnv LC_IDENTIFICATION LC_ALL LANGUAGE
+AcceptEnv XMODIFIERS
+Subsystem sftp  /usr/libexec/openssh/sftp-serve
+EOF
+
+chmod 0600 /etc/ssh/sshd_config
+
+echo 'net.ipv6.conf.all.disable_ipv6 = 1' >> '/etc/sysctl.conf'
+echo 'net.ipv6.conf.default.disable_ipv6 = 1' >> '/etc/sysctl.conf'
+
+cat > /etc/sudoers.d/zuul <<EOF
+Defaults    !requiretty
+zuul-worker ALL=(ALL) NOPASSWD:ALL
+zuul ALL=(ALL) NOPASSWD:ALL
+EOF
+
+chmod 0440 /etc/sudoers.d/zuul
+
+cat > /etc/network/interfaces.d/auto <<EOF
+# The primary network interface
+allow-hotplug enp1s0
+iface enp1s0 inet dhcp
+EOF
+
+# zuul.sh ends here
