6344e43d3aa7f10a0121043f6495385257f9a499
Frederic Lepied <flepied@wazo.io>
2019-08-27 09:28:56 +0200

    nodepool/elements/virt-customize/zuul.sh: do not change sshd_config

--- nodepool/elements/virt-customize/zuul.sh
+++ nodepool/elements/virt-customize/zuul.sh
@@ -54,31 +54,6 @@ echo 'PATH="/home/zuul-worker/.local/bin:$PATH"' >> /home/zuul-worker/.profile
 mkdir -p /home/zuul-worker/.local/bin
 chown -R zuul-worker:zuul-worker /home/zuul-worker
 
-cat > /etc/ssh/sshd_config <<EOF
-HostKey /etc/ssh/ssh_host_rsa_key
-HostKey /etc/ssh/ssh_host_ecdsa_key
-HostKey /etc/ssh/ssh_host_ed25519_key
-KexAlgorithms curve25519-sha256@libssh.org,ecdh-sha2-nistp521,ecdh-sha2-nistp384,ecdh-sha2-nistp256,diffie-hellman-group-exchange-sha256
-Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
-MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com
-SyslogFacility AUTHPRIV
-AuthorizedKeysFile .ssh/authorized_keys
-PasswordAuthentication no
-ChallengeResponseAuthentication no
-GSSAPIAuthentication no
-GSSAPICleanupCredentials no
-UsePAM yes
-X11Forwarding no
-UseDNS no
-AcceptEnv LANG LC_CTYPE LC_NUMERIC LC_TIME LC_COLLATE LC_MONETARY LC_MESSAGES
-AcceptEnv LC_PAPER LC_NAME LC_ADDRESS LC_TELEPHONE LC_MEASUREMENT
-AcceptEnv LC_IDENTIFICATION LC_ALL LANGUAGE
-AcceptEnv XMODIFIERS
-Subsystem sftp  /usr/libexec/openssh/sftp-serve
-EOF
-
-chmod 0600 /etc/ssh/sshd_config
-
 echo 'net.ipv6.conf.all.disable_ipv6 = 1' >> '/etc/sysctl.conf'
 echo 'net.ipv6.conf.default.disable_ipv6 = 1' >> '/etc/sysctl.conf'
 
